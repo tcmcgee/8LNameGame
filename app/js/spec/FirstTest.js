fe70999ee9ae616e46a2e98a0fe14f0f8e29414c
nystromb <bnystrom@8thlight.com>
2016-04-04 11:48:52 -0500

    Remove extra cruft

--- app/js/spec/FirstTest.js
+++ app/js/spec/FirstTest.js
@@ -1,10 +1,6 @@
 describe("Extenson", function(){
   var nameGame = new NameGame();
 
-  it ("should be able to pass a test", function() {
-      expect(true).toEqual(true);
-  });
-
   it ("should be able to check if an array of strings contains a value", function(){
     var stringArray = ["Hello", "My", "Name", "Is", "Tom"]
 
